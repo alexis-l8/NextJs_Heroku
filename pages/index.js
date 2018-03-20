ef953022f13e247ca3588ace9742ef16a61d68e9
Alexis Lui <alexislui@hotmail.co.uk>
2018-03-20 18:16:09 +0000

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,18 @@
-import React from 'react'
-import Head from 'next/head'
-import Logo from '../components/logo'
+const Index = () => (
+  <div>
+    <h1>Hello World!!</h1>
+    <p>Insert your email here please</p>
 
-export default () => (
-  <div className="root">
-    <Head>
-      <meta charSet="utf-8"/>
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <title>Next.js on Heroku</title>
-    </Head>
-    <style jsx>{`
-      .root {
-        font-family: sans-serif;
-        line-height: 1.33rem;
-        margin-top: 8vh,
-      }
-      @media (min-width: 600px) {
-        .root {
-          margin-left: 21vw;
-          margin-right: 21vw;
-        }
-      }
-    `}</style>
-
-    <h1><Logo style={{ height: '1.45rem' }}/> Next.js on Heroku</h1>
-
-    <p>!!Deploy <a href="https://zeit.co/blog/next">Next.js</a> universal web apps on <a href="https://www.heroku.com/home">Heroku</a>.</p>
-
-    <p>This <strong>demo deployment on Heroku</strong> is from the repo <a href="https://github.com/mars/heroku-nextjs">mars/heroku-nextjs</a>.</p>
-
-    <p><a href="https://github.com/mars/heroku-nextjs/archive/master.zip">Download this Next.js app</a> as a Heroku-ready template, or follow <a href="https://github.com/mars/heroku-nextjs#production-deployment">Production Deployment</a> to push an existing app to Heroku.</p>
+    <form action="/api/mailing-list/submit" method="post">
+      First Name
+      <input type="text" name="first_name" />
+      Last Name
+      <input type="text" name="last_name" />
+      Email
+      <input type="text" name="email" />
+      <input type="submit" />
+    </form>
   </div>
 )
+
+export default Index
