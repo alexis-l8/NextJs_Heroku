7557ab1b023faa614bcbdc359b06b5651d15e016
Alexis Lui <alexislui@hotmail.co.uk>
2018-03-20 19:26:58 +0000

    server to start

--- start.js
+++ start.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const next = require('next')
+const bodyParser = require('body-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000
 
 app.prepare()
 .then(() => {
-  const server = express()
+  const app = express()
 
-  server.get('*', (req, res) => {
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  app.get('*', (req, res) => {
     return handle(req, res)
   })
 
-  server.listen(process.env.PORT || 3000, (err) => {
+  app.listen(port, err => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on port %d', port)
   })
 })
 .catch((ex) => {
