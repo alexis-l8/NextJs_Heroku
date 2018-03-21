07bd63ec01ec613d5881309d3c9f129e43c67a98
Alex Wong <wong_ho_ming@hotmail.com>
2018-03-21 17:22:36 +0000

    added the dollar in yml

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,6 +1,6 @@
 describe('Home page', () => {
   it('visits home page & submit form with data', () => {
-    cy.visit('/')
+    cy.visit(Cypress.env('URL'))
     cy.get('form input[name="first_name"]').type('cypress')
     cy.get('form input[name="last_name"]').type('test')
     cy.get('form input[name="email"]').type(`${new Date()}@test.com`)
