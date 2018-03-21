413da7559dc95fd211ee448e5358e2f65d97923e
Alex Wong <wong_ho_ming@hotmail.com>
2018-03-21 16:42:20 +0000

    changed config and test files

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,6 +1,6 @@
 describe('Home page', () => {
   it('visits home page & submit form with data', () => {
-    cy.visit(Cypress.env('URL'))
+    cy.visit(Cypress.env('HOST') || 'index.html')
     cy.get('form input[name="first_name"]').type('cypress')
     cy.get('form input[name="last_name"]').type('test')
     cy.get('form input[name="email"]').type(`${new Date()}@test.com`)
