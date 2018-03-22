fe5169b836313cdeadf293c6b4a11a86fef344b3
Alex Wong <wong_ho_ming@hotmail.com>
2018-03-22 11:49:54 +0000

    added background to true on yml

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -2,7 +2,7 @@ describe('Home page', () => {
   it('visits home page & submit form with data', () => {
     console.log('url', process.env.URL)
     console.log('url cypress', Cypress.env('URL'))
-    cy.visit('https://circleci.com/gh/wonghoming/NextJs_Heroku/27')
+    cy.visit('https://circleci.com/gh/wonghoming/NextJs_Heroku/31')
     cy.get('form input[name="first_name"]').type('cypress')
     cy.get('form input[name="last_name"]').type('test')
     cy.get('form input[name="email"]').type(`${new Date()}@test.com`)
