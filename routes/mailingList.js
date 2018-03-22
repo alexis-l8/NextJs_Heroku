e8c71358a9d96fdc3983f1579319808443d0f8e7
Alexis Lui <alexislui@hotmail.co.uk>
2018-03-22 15:35:10 +0000

    all x fordward for ip header

--- routes/mailingList.js
+++ routes/mailingList.js
@@ -4,7 +4,7 @@ const insertEmail = require('../db/query/insertEmail')
 
 mailingListRoute.post('/submit', (req, res) => {
   const { first_name, last_name, email } = req.body
-  const IP = req.headers["X-Forwarded-For"] ? req.headers["X-Forwarded-For"].split(',').pop() : req.connection.remoteAddress
+  const IP = req.headers["X-Forwarded-For"]
   insertEmail(first_name, last_name, email, IP) .then(() => {
   res.status(201).redirect('/')
   })
