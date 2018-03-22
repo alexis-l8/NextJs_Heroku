d3197a9d9e2e5356e49206173ea130492c266282
Alex Wong <wong_ho_ming@hotmail.com>
2018-03-22 11:18:57 +0000

    changed to process.env

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -2,7 +2,7 @@ describe('Home page', () => {
   it('visits home page & submit form with data', () => {
     console.log('url', process.env.URL)
     console.log('url cypress', Cypress.env('URL'))
-    cy.visit(Cypress.env('URL'))
+    cy.visit(process.env.URL)
     cy.get('form input[name="first_name"]').type('cypress')
     cy.get('form input[name="last_name"]').type('test')
     cy.get('form input[name="email"]').type(`${new Date()}@test.com`)
